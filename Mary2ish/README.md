9ea4091ed2896e0edd08485139e869ae43710361
Pestilent <pestilent@amaterasu>
2025-06-30 15:56:03 -0400

    Initial setup

---
+++ Mary2ish/README.md
@@ -0,0 +1,2 @@
+# MARY 2.0ish
+Trying to create a web based chat interface with LLM flexability and MCP support.
---
+++ fast-agent testing/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fast-agent-testing!")
+
+
+if __name__ == "__main__":
+    main()
